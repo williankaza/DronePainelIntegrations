9f36758b57822a5c047c1ee66dae3c46833b0eff
williankaza <williankaza@hotmail.com>
2020-10-26 17:59:00 -0300

    Criando tela de Monitoramento com google-maps lib

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { AppComponent } from './app.component';
 import { ConsultaComponent } from './drone/consulta/consulta.component';
 import { CadastroComponent } from './drone/cadastro/cadastro.component';
 import { MonitoramentoComponent } from './monitoramento/monitoramento/monitoramento.component';
+import { MonitoramentoV2Component } from './monitoramento-v2/monitoramento-v2.component';
 
 const routes: Routes = [
   {
     path: 'monitoramento',
     component: MonitoramentoComponent
+  },
+  {
+    path: 'monitoramentoV2',
+    component: MonitoramentoV2Component
   },
   {
     path: 'drone',
