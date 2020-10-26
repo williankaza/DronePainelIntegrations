bf11e119faec29735266876f698c46a5b64c5f7a
ArunaMartins <aruna.martins@creditas.com.br>
2020-10-26 19:47:23 -0300

    editing css style

--- src/app/drone/consulta/consulta.component.ts
+++ src/app/drone/consulta/consulta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PoTableColumn, PoTableAction } from '@po-ui/ng-components';
+
 
 @Component({
   selector: 'app-consulta',
@@ -7,9 +9,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultaComponent implements OnInit {
 
-  constructor() { }
+//   constructor() { }
 
   ngOnInit(): void {
   }
+//     gridCols: Array<PoTableColumn> = [
+//       {
+//         label: 'id',
+//         property: 'id',
+//         //visible: false
+//       },
+//       {
+//         label:'Acompanhar',
+//         type: 'button'
+//       }
+//     ]
 
+//     gridActions: Array<PoTableAction> = [
+//       {
+//         label: 'Acompanhar',
+//         action: this.insereDroneMap.bind(this)
+//       }
+//     ]
+  
+//     listDrones: Array<Drones> = [
+//       {
+//         id: '00000001',
+//         latitude: 61.673858,
+//         longitude: 27.815982, 
+//         temperatura: -10,
+//         umidade: 450
+//       },
+//       {
+//         id: '00000002',
+//         latitude: 41.673858,
+//         longitude: 7.815982, 
+//         temperatura: -10,
+//         umidade: 450
+//       },
+//       {
+//         id: '00000003',
+//         latitude: 1.673858,
+//         longitude: 57.815982, 
+//         temperatura: -10,
+//         umidade: 450
+//       },
+//       {
+//         id: '00000004',
+//         latitude: 11.673858,
+//         longitude: -27.815982, 
+//         temperatura: -10,
+//         umidade: 450
+//       }
+//     ]
+//     }
 }
+
